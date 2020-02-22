a35c73442ddf80d1a7b06e2fe9eeef508a8756ce
jan <jan@kwoh.de>
2020-02-22 15:38:59 +0100

    dont clear keychain on start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 
 set -gx HOSTNAME (hostname)
 if status --is-interactive;
-    keychain --quiet --nogui --clear ~/.ssh/id_rsa ~/.ssh/tide-jan
+    keychain --quiet --nogui ~/.ssh/id_rsa ~/.ssh/tide-jan
     [ -e $HOME/.keychain/$HOSTNAME-fish ]; and source $HOME/.keychain/$HOSTNAME-fish
 end
 
