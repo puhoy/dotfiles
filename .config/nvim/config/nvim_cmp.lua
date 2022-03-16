cd8ef8924ab6f580b25cb1d1a98fe39ad48a0f79
jan <jan@kwoh.de>
2022-03-16 23:56:49 +0100

    nvim cmp complete on return

--- .config/nvim/config/nvim_cmp.lua
+++ .config/nvim/config/nvim_cmp.lua
@@ -22,7 +22,8 @@
       ['<C-e>'] = cmp.mapping.close(),
       --['<CR>'] = cmp.mapping.confirm({ select = true }),
       ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
+      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     },
     sources = {
       { name = 'nvim_lsp' },
