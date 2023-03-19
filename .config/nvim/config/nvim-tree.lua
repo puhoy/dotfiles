e0939ac3d856b4cf5ad82b310720b4ad26cdfa23
jan <jan@kwoh.de>
2023-03-19 16:15:16 +0100

    update nvimtree

--- .config/nvim/config/nvim-tree.lua
+++ .config/nvim/config/nvim-tree.lua
@@ -12,19 +12,20 @@ require'nvim-tree'.setup {
   -- hijack the cursor in the tree to put it at the start of the filename
   hijack_cursor       = true,
   -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = false,
+  update_cwd          = true,
   -- show lsp diagnostics in the signcolumn
   diagnostics     = {
       enable = true,
   },
+  prefer_startup_root = true,
   -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
   update_focused_file = {
     -- enables the feature
     enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = true,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+
+    -- update root when changing to a file outside workdir
+    update_root = true,
+
     -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
     ignore_list = {}
   },
